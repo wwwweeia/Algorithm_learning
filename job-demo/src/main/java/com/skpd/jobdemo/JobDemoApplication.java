bf0dab50351c2c49d1f6abf10ad0a2222bc33049
wang_quanwei <wwwweia@163.com>
2022-05-10 21:39:40 +0800

    11

---
+++ job-demo/src/main/java/com/skpd/jobdemo/JobDemoApplication.java
@@ -0,0 +1,13 @@
+package com.skpd.jobdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDemoApplication.class, args);
+	}
+
+}
